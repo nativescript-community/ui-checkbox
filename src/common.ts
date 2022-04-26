ea27f6093cf2d02b4e76c5af192ef13c5c4e8826
Martin Guillon <martin.guillon@akylas.fr>
2022-04-26 14:04:08 +0200

    chore: move to @nativescript-community

--- src/common.ts
+++
@@ -1,4 +0,0 @@
-export enum BoxType {
-  circle = 'circle',
-  square = 'square'
-}
--- src/references.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference path="./typings/BEMCheckBox.d.ts" />
-/// <reference path="./node_modules/@nativescript/types/index.d.ts" />
