4ed576498c28a819e690d292ec302f0aaa67a43a
Martin Guillon <dev@akylas.fr>
2026-01-06 09:45:26 +0000

    readme/doc

--- docs/assets/navigation.js
+++ docs/assets/navigation.js
@@ -1,1 +1,1 @@
-window.navigationData = "eJyLrlYqSa0oUbJScs5ITc52yq9Q0lEqSCzJULJSSs5JLC5OLdaHyehllOTmKOkoZWfmpShZGRpZ1Opg6PbMK0ktSktMTkUYkwkTQpgEV4VqpJGpWW0sABkdMWU="
+window.navigationData = "eJyLjgUAARUAuQ=="
